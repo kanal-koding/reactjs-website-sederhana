80e40aa2f655c7c4a7c5410877f187c0479cfa52
kanalkoding <kanalkoding@gmail.com>
2024-05-28 11:18:30 +0700

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import '../styles/Footer.css'
+
+function Footer() {
+  return (
+    <div id="copyright">
+        <div className="wrapper">
+            &copyt; 2024. <b>Kanal Koding.</b> All Rights Reserved.
+        </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/data/ContactSection.jsx
@@ -0,0 +1,13 @@
+export const contactSection = [
+    {
+        content:`<h3>Kanal Koding.</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, culpa!</p>`
+    },
+    {content:` <h3>About</h3>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, culpa!</p>`},
+    {content:`<h3>Contact</h3>
+    <p>Jl Raya Seputih Banyak Lampung Tengah</p>
+    <p>Kode Pos: 34156</p>`},
+    {content:` <h3>Social</h3>
+    <p><b>YouTube: </b>Kanal Koding</p>`}
+]
